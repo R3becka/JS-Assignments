8d4a9f59a3e646902b31d504d3379afb88f35be9
R3becka <rebhel464@ihmeducation.se>
2024-09-12 13:10:33 +0200

    Updated P4

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -1,6 +1,6 @@
 let numberOfBottles = 10;
-const line1 = ("green bottles hanging on the wall");
-const line2 = ("and if one green bottle should accidentally fall, there will be");
+const line1 = "green bottles hanging on the wall";
+const line2 = "and if one green bottle should accidentally fall, there will be";
 
 console.log(numberOfBottles);
 console.log(line1);
