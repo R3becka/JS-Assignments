ada54603dc188ffb3bdaee60347cd9e2d80af496
R3becka <rebhel464@ihmeducation.se>
2024-09-12 14:11:16 +0200

    Added basic operators

---
+++ P5Operators/js/app.js
@@ -0,0 +1,55 @@
+// let x = 5 + 5; //10
+// let y = x + 5; //15
+// let z = y + y; //z=60
+// console.log(z +z);
+//
+// //Practice two
+//
+// let FirstNumber = 70;
+// let secondNumber = 20;
+// let seven = 7;
+// let answer = 0;
+//
+// answer= FirstNumber + secondNumber; //You take what you want to store the result in (answer) & add what you want to store (one & two)
+// console.log(answer)
+//
+// answer = FirstNumber - secondNumber;
+// console.log(answer);
+//
+// answer = FirstNumber / secondNumber;
+// console.log(answer);
+//
+// answer = FirstNumber / secondNumber;
+// console.log(answer);
+//
+// //Practice three
+// console.log("Practice three");
+//
+// answer = FirstNumber % secondNumber;
+// console.log(answer);
+
+
+//Practice 4
+console.log("Practice 4");
+
+let gold = 50;
+gold + 10;
+console.log(gold);
+
+gold = gold + 10; //This is too long of a code
+console.log(gold);
+
+gold += 10; //This is the way to do it
+console.log(gold); //60
+
+gold *= 10;
+console.log(gold); //700
+
+gold /= 10;
+console.log(gold);
+
+gold -= 10;
+console.log(gold);
+
+gold %= 2;
+console.log(gold);
