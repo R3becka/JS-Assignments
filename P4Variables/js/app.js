befb8c6375e2044076990947af5015633fd2d8aa
R3becka <rebhel464@ihmeducation.se>
2024-09-12 11:35:18 +0200

    Changed Variables

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -5,7 +5,10 @@ const line2 = ("and if one green bottle should accidentally fall, there will be"
 
 console.log(numberOfBottles);
 console.log(line1);
+console.log(numberOfBottles);
+console.log(line1);
 console.log(line2);
-console.log(numberOfBottles - 1);
+console.log(--numberOfBottles);
+console.log(line1)
 
 
