b4be539f621e29038143d814aaefedf677ded0a7
R3becka <rebhel464@ihmeducation.se>
2024-09-16 13:10:11 +0200

    Created P8

--- P7-Boolean/js/app.js
+++ P7-Boolean/js/app.js
@@ -1,14 +1,4 @@
-// Output:You are a child:False
-// Output:You are a teenager: False
-// Output:You are an adult: True
-// Start with a variable
-// const birthYear = 2000;
-// Do a few age-checks:
-// Save to a letvariable named isChild, when the age is from 0 till 10.
-// Save to a letvariable named isTeenager, when the age is from 11 to 17
-// Save to a letvariable named isAdult, when the age is 18 or more.
-//   Then, print them all to the console.
-//   Try with different birth years and account for edge cases (if birthYear is 2030?)
+
 
 const birthYear = 2010;
 const currentYear = 2024
@@ -39,3 +29,5 @@ else {
   console.log("Player is NOT on the ground!");
     console.log("Player CAN'T jump");
 }
+
+
---
+++ P8ConditionalBranching/js/app.js
@@ -0,0 +1,17 @@
+const birthYear = 2010;
+const currentYear = 2024;
+let ageOfPerson = currentYear - birthYear;
+console.log(ageOfPerson);
+
+// Save to a let variable named isChild, when the age is from 0 till 10.
+let isChild = (ageOfPerson >= 0) && (ageOfPerson <= 10);
+
+// Save to a let variable named isTeenager, when the age is from 11 to 17
+let isTeenager = (ageOfPerson >= 11) && (ageOfPerson <= 17);
+
+// Save to a let variable named isAdult, when the age is 18 or more.
+let isAdult =  ageOfPerson >= 18;
+
+console.log(isChild);
+console.log(isTeenager);
+console.log(isAdult);
