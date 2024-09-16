11c971c9644f6a307f03120db2d1f590354cc1dc
R3becka <rebhel464@ihmeducation.se>
2024-09-16 14:00:05 +0200

    Updated with new code

--- P8ConditionalBranching/js/app.js
+++ P8ConditionalBranching/js/app.js
@@ -1,6 +1,6 @@
 const birthYear = 2010;
 const currentYear = 2024;
-const favoriteNumber = 15;
+const favoriteNumber = 14;
 let ageOfPerson = currentYear - birthYear;
 console.log(ageOfPerson);
 
@@ -13,14 +13,13 @@ let isTeenager = (ageOfPerson >= 11) && (ageOfPerson <= 17);
 // Save to a let variable named isAdult, when the age is 18 or more.
 let isAdult =  ageOfPerson >= 18;
 
+
 if (isChild) {
   console.log("You are a child.");
 }
-
 else if (isTeenager) {
   console.log("You are a teenager.");
 }
-
 else if (isAdult) {
   console.log("You are an adult.");
 }
@@ -28,10 +27,16 @@ console.log()
 if (favoriteNumber > favoriteNumber) {
   console.log("Favorite Number is greater");
 }
-
 else if (favoriteNumber === ageOfPerson) {
   console.log("They are the same")
 }
 else {console.log("age is greater")
 }
-
+if (favoriteNumber % 3 === 0) {
+  console.log("Favorite Numbers is Odd")
+}
+else {
+  console.log("Favorite Number is Even")
+}
+//let result = condition ? thenValue : elseValue är koden nedan
+favoriteNumber % 3 === 0 ? console.log("Favorite Number is odd") : console.log("Favorite Number is even")
