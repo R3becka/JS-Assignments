29bad780a48b6b0bc79becdce7bf1bb0cdf71dd5
R3becka <rebhel464@ihmeducation.se>
2024-09-19 11:25:39 +0200

    CreatedButtonThatIncreasesNumber

--- NewProject13/js/app.js
+++ NewProject13/js/app.js
@@ -1,19 +1,43 @@
 
 // We link (assign) the paragraph "text0" from index to texFile0
-const textField0 = document.getElementById("text0");
+// const textField0 = document.getElementById("text0");
+const textField1 = document.getElementById("text1");
 
 
-//We call the text0 (aka textField0 and assigns it to a new textString
-textField0.innerHTML = showStylizedScore(786);
 
-function showScore(inputNumber) {
-  return inputNumber * 100;
+const IncreaseScoreButton = document.getElementById("button0");
+//Internal Variables
+let score = 0;
+
+
+//Process (what is going to happen in what sequence)
+IncreaseScoreButton.addEventListener("click", () => {
+  increaseScoreByOne()
+  updateScoreText()
+checkScoreForSeven()
+});
+
+
+
+//controllers
+function increaseScoreByOne () {
+  score++;
+  updateScoreText();
 }
-//in function getRandomInt() {
-//   return Math.floor(Math.random() * 3);
-// }this function above output is a number
 
-function showStylizedScore(scoreInput) {
-  return "Your score is: " + scoreInput * 100;
+function checkScoreForSeven (){
+  if (score >= 7) {
+    {changeScoreTextColor();}
+  }
+
 }
-//in this function above a string + a number is output
+
+//View
+function updateScoreText () {
+  textField1.innerHTML = "Your score is: " + score;
+}
+
+function changeScoreTextColor (){
+  textField1.style.color ="green";
+
+}
