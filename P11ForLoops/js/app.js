41ea390696cb9487176d68a872e6cdde99cfd1b9
R3becka <rebhel464@ihmeducation.se>
2024-09-23 11:22:16 +0200

    practiced for

--- P11ForLoops/js/app.js
+++ P11ForLoops/js/app.js
@@ -1,1 +1,44 @@
-p
+// for (let i = 0; i < 13; i++) {
+//   console.log(getRandomInt(6) + 1);
+// }
+//
+//
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+// }
+//
+// for (let i = 1; i <= 1024; i *= 2) {
+//   console.log(i);
+// }
+//
+// for (let i = 10; i >= 0; i--) {
+//   console.log(i);
+// }
+//
+// function power (firstNumber, exponent) {
+//   //Here we cache the result.
+//   let result = firstNumber;
+//   for (let i = 1; i < exponent; i++) {
+//     //Here we multiply the result by the firstNumber (e.g. 8*2)
+//   result *= firstNumber;
+// }
+//   //Once the loop is over, we return the result
+//   return result;
+// }
+// console.log(power(4 , 24));
+//
+// console.log(Math.pow(4, 24));
+//
+//
+// for (let i = 12; i >= 0 ; i--) {
+//   console.log(i + " green bottles hanging on the wall" +i+" green bottles hanging on the wall"
+//     + " And if one green bottle should accidentally fall there will be " + i +
+//     " green bottles hanging on the wall");
+// }
+
+
+let item1;
+let item2;
+let item3;
+let item4;
+
